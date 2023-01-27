94e85ad206d98c7d7a0bded7badd3f356402f2cf
Shivam Shukla <shivam@gkmit.co>
2023-01-27 13:00:25 +0530

    created routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-function App() {
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
+import { Home } from "./pages/home";
+import { Store } from "./pages/store";
+import { About } from "./pages/about";
+
+function App() {
   return (
-    <h1>Hello</h1>
-  )
+    <Container className="mb-4">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Container>
+  );
 }
 
-export default App
+export default App;
